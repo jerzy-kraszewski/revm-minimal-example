d8a914cd7094d5930357a8607dc65b1bd8c2a6a6
jkraszewski <jkraszewski@match-trade.com>
2025-01-17 17:56:13 +0100

    counter read number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,62 @@
+use std::sync::Arc;
+
+use alloy::{eips::BlockId, providers::ProviderBuilder, sol_types::SolCall};
 use anyhow::Result;
+use log::info;
+use revm::{
+    db::{AlloyDB, CacheDB},
+    primitives::{address, AccountInfo, Bytecode, TransactTo},
+    Evm,
+};
+use revm_minimal_example::{
+    models::{parse_result, Counter},
+    utils::setup_logger,
+};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    dotenvy::dotenv().ok();
+    setup_logger()?;
+
+    let sender = address!("0xaaaaaaa0B01a61e6B8609482608493a6748d1781");
+    let rpc_url = std::env::var("RPC_URL").unwrap();
+    info!("RPC URL: {}", rpc_url);
+
+    let provider = ProviderBuilder::new().on_builtin(&rpc_url).await?;
+    let provider = Arc::new(provider);
+    let mut db = CacheDB::new(AlloyDB::new(provider.clone(), BlockId::default()).unwrap());
+
+    let counter_bytecode = Bytecode::new_raw(Counter::BYTECODE.clone());
+    let counter_address = address!("0x1234567890abcdef2ec9065E61B39BB9E4d82513");
+    let counter_account = AccountInfo::from_bytecode(counter_bytecode);
+    db.insert_account_info(counter_address, counter_account);
+
+    let evm = Evm::builder().with_ref_db(db).build();
+
+    let number_call = Counter::numberCall {};
+    let calldata = Counter::numberCall::abi_encode(&number_call);
+
+    let mut evm = evm
+        .modify()
+        .modify_tx_env(|tx| {
+            tx.caller = sender;
+            tx.transact_to = TransactTo::Call(counter_address);
+            tx.data = calldata.into();
+        })
+        .build();
+
+    let result = evm.transact();
+    let tx_result = parse_result(result.unwrap().result)?;
+
+    let number = match Counter::numberCall::abi_decode_returns(&tx_result.output, true) {
+        Ok(n) => n,
+        Err(e) => {
+            panic!("Error decoding number: {}", e);
+        }
+    };
+    let number = number._0;
+
+    info!("Counter number: {}", number);
+
     Ok(())
 }
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,49 @@
 use alloy::sol;
+use anyhow::{anyhow, Result};
+use revm::primitives::{Bytes, ExecutionResult, Log, Output};
 
 sol!(Counter, "contracts/out/Counter.sol/Counter.json");
+
+#[derive(Debug, Clone)]
+pub struct TxResult {
+    pub output: Bytes,
+    pub logs: Option<Vec<Log>>,
+    pub gas_used: u64,
+    pub gas_refunded: u64,
+}
+
+pub fn parse_result(execution_result: ExecutionResult) -> Result<TxResult> {
+    let tx_result = match execution_result {
+        ExecutionResult::Success {
+            gas_used,
+            gas_refunded,
+            output,
+            logs,
+            ..
+        } => match output {
+            Output::Call(o) => TxResult {
+                output: o,
+                logs: Some(logs),
+                gas_used,
+                gas_refunded,
+            },
+            Output::Create(o, _) => TxResult {
+                output: o,
+                logs: Some(logs),
+                gas_used,
+                gas_refunded,
+            },
+        },
+        ExecutionResult::Revert { gas_used, output } => {
+            return Err(anyhow!(
+                "EVM REVERT: {:?} / Gas used: {:?}",
+                output,
+                gas_used
+            ))
+        }
+        ExecutionResult::Halt {
+            reason, gas_used, ..
+        } => return Err(anyhow!("EVM HALT: {:?} / Gas used: {:?}", reason, gas_used)),
+    };
+    Ok(tx_result)
+}
